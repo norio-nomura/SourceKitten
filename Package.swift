8d92303cd6ba82803a2cb95646c6840e6e6f7e99
Norio Nomura <norio.nomura@gmail.com>
2016-08-31 19:22:49 +0900

    Update SWXMLHash to norio-nomura
    Tagged commit is already merged to drmohundro’s xcode-8.0 branch.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
   ],
   dependencies: [
     .Package(url: "https://github.com/norio-nomura/Commandant.git", "0.11.0-alpha.1"),
-    .Package(url: "https://github.com/jpsim/SWXMLHash.git", Version(2, 4, 5)),
+    .Package(url: "https://github.com/norio-nomura/SWXMLHash.git", "2.6.0-alpha.1"),
     .Package(url: "https://github.com/jpsim/YamlSwift.git", Version(1, 6, 2)),
     .Package(url: "https://github.com/norio-nomura/Clang_C.git", majorVersion: 1),
     .Package(url: "https://github.com/norio-nomura/SourceKit.git", majorVersion: 1),
